a9ff36f9f6c7d35d32a1872cc12e7bafc2a75c36
Ramandeep Singh <raman.soin2004@gmail.com>
2025-03-12 23:11:02 +0530

    optimized similarity search code

--- 3.EmbeddingModels/4_document_similarity.py
+++ 3.EmbeddingModels/4_document_similarity.py
@@ -17,17 +17,22 @@ documents = [
 
 query = 'tell me about bumrah'
 
-doc_embeddings = embedding.embed_documents(documents)
-query_embedding = embedding.embed_query(query)
+doc_embeddings = np.array(embedding.embed_documents(documents))
+query_embedding = np.array(embedding.embed_query(query)).reshape(1,384)
 
-scores = cosine_similarity([query_embedding], doc_embeddings)[0]
+sim_score = cosine_similarity(doc_embeddings,query_embedding)
 
-index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
-
-print(query)
-print(documents[index])
-print("similarity score is:", score)
+# Optimized Code
+print(np.argmax(sim_score)) # prints the index with maximum similarity score
+print(documents[np.argmax(sim_score)]) # prints the most similar sentence
 
 
+'''****************************'''
 
+# Code in Youtube Lecture
 
+# scores = cosine_similarity([query_embedding], doc_embeddings)[0]
+# index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+# print(query)
+# print(documents[index])
+# print("similarity score is:", score)
